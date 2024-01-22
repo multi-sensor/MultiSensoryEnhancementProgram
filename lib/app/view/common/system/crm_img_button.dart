ece014521a7a4dbd3e8025e0cc48a2ec05ba7996
alimhanhan <alim11@naver.com>
2024-01-22 15:18:48 +0900

    feat:modify dialog

--- lib/app/view/common/system/crm_img_button.dart
+++ lib/app/view/common/system/crm_img_button.dart
@@ -19,73 +19,73 @@ class CRMImgButton extends StatelessWidget {
 
     return GestureDetector(
       onTap: () => needLevel ?
-        showDialog(
-          context: context,
-          builder: (context) => Dialog(
-            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12.0)),
-            child: Container(
-              padding: const EdgeInsets.all(30),
-              height: 450, width: 500,
-              decoration: BoxDecoration(borderRadius: BorderRadius.circular(16.0), color: AppColors.sub1Color),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Align(
-                    alignment: Alignment.topRight,
-                    child: ElevatedButton(
-                      onPressed: () => Navigator.pop(context),
-                      style: ElevatedButton.styleFrom(
-                        foregroundColor: Colors.black, backgroundColor: AppColors.sub2Color,
-                      ),
-                      child: const Text('X'),
+      showDialog(
+        context: context,
+        builder: (context) => Dialog(
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12.0)),
+          child: Container(
+            padding: const EdgeInsets.all(30),
+            height: 450, width: 500,
+            decoration: BoxDecoration(borderRadius: BorderRadius.circular(16.0), color: AppColors.sub1Color),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Align(
+                  alignment: Alignment.topRight,
+                  child: ElevatedButton(
+                    onPressed: () => Navigator.pop(context),
+                    style: ElevatedButton.styleFrom(
+                      foregroundColor: Colors.black, backgroundColor: AppColors.sub2Color,
                     ),
+                    child: const Text('X'),
                   ),
-                  const SizedBox(height: 20),
-                  Expanded(
-                    child: GridView.builder(
-                      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 2,
-                        childAspectRatio: 2.0,
-                      ),
-                      itemCount: AppValues.fileData['levelTitle'].length, // 리스트의 길이를 사용
-                      itemBuilder: (context, index) {
-                        return Container(
-                          margin: const EdgeInsets.all(10),
-                          decoration: BoxDecoration(
-                            color: AppColors.sub2Color,
-                            borderRadius: BorderRadius.circular(30),
-                          ),
-                          child: TextButton(
-                            child: CRMText(
+                ),
+                const SizedBox(height: 20),
+                Expanded(
+                  child: GridView.builder(
+                    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      childAspectRatio: 2.0,
+                    ),
+                    itemCount: AppValues.fileData['levelTitle'].length, // 리스트의 길이를 사용
+                    itemBuilder: (context, index) {
+                      return Container(
+                        margin: const EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                          color: AppColors.sub2Color,
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        child: TextButton(
+                          child: CRMText(
                               textContent: AppValues.fileData['levelTitle'][index] ?? '',
                               fontSize: 16,// 리스트에서 제목 가져오기
                               fontStyle: AppTextThemes.cookieRunStyle
-                            ),
-                            onPressed: (){
-                              Navigator.pushNamed(
-                                context,
-                                '/contents',
-                                arguments: {'level': index, 'category': imageIdx},
-                              );
-                            },
                           ),
-                        );
-                      },
-                    ),
+                          onPressed: (){
+                            Navigator.pushNamed(
+                              context,
+                              '/contents',
+                              arguments: {'level': index, 'category': imageIdx},
+                            );
+                          },
+                        ),
+                      );
+                    },
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
-        ) : Navigator.pushNamed(
-          context,
-          '/contents',
-          arguments: {'level': level, 'category': imageIdx},
+        ),
+      ) : Navigator.pushNamed(
+        context,
+        '/contents',
+        arguments: {'level': level, 'category': imageIdx},
       ),
       child: AspectRatio(
         aspectRatio: 1, // 정사각형
         child: Container(
-          padding: title == "기차 만들기" ? EdgeInsets.only(left:0.0, right:0.0, bottom: 20.0, top:0.0) : const EdgeInsets.all(20),
+          padding: title == "기차 만들기" ? EdgeInsets.only(left:20.0, right:0.0, bottom: 20.0, top:20.0) : const EdgeInsets.all(20),
           decoration: BoxDecoration(
             color: AppColors.white,
             borderRadius: BorderRadius.circular(15),
@@ -99,15 +99,12 @@ class CRMImgButton extends StatelessWidget {
                 child: Align(
                   alignment: Alignment.centerRight,
                   child: Image.asset(imagePath),
-              ),
-              ),
-              Padding(
-                padding: title == "기차 만들기" ? EdgeInsets.only(top: 20.0, bottom: 0.0) : const EdgeInsets.all(0),
-                child: CRMText(
-                  textContent: title,
-                  fontSize: 20,
-                  fontStyle: AppTextThemes.cookieRunWhiteStyle,
                 ),
+              ),
+              CRMText(
+                textContent: title,
+                fontSize: 20,
+                fontStyle: AppTextThemes.cookieRunWhiteStyle,
               ),
             ],
           ),
@@ -116,4 +113,3 @@ class CRMImgButton extends StatelessWidget {
     );
   }
 }
-
